f1a9ce19b9e949df31d6ccc8600239fe50de0bca
Sylvia Makuch <sylvia.makuch@protonmail.com>
2022-03-18 13:39:57 -0700

    added routes to the app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import React from "react";
+import styled from "styled-components";
 import Categories from "./components/Categories.js";
 import Search from "./components/Search.js";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+const PageContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+  flex-wrap: wrap;
+  flex-direction: column;
+`;
+
+const H1 = styled.h1`
+  font-size: 88px;
+  font-family: "Inspiration", cursive;
+`;
 
 function App() {
   return (
-    <div>
-      <h1>The CookBook</h1>
-      <Search />
-      <Categories />
-    </div>
-
+    <PageContainer>
+      <H1>The CookBook</H1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/search" element={<Search />} />
+          <Route path="catergories" element={<Categories />} />
+        </Routes>
+      </BrowserRouter>
+    </PageContainer>
   );
 }
 
--- src/components/Search.js
+++ src/components/Search.js
@@ -2,27 +2,49 @@ import React from "react";
 import { useState } from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import axios from "axios";
+import forkspoon from "./../media/forkspoon.svg";
+import styled from "styled-components";
+
+const PageContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+  flex-wrap: wrap;
+  flex-direction: column;
+`;
+
+const ForkSpoonImg = styled.img`
+  width: 70%;
+  margin: 50px;
+`;
 
 export default function Search() {
   const [isSearched, isSetSearched] = useState("");
-  
+
   const submitHandler = async (e) => {
     e.preventDefault();
     console.log(isSearched);
-    const res =  await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${isSearched}`);
-    console.log(res.data);
+    const res = await axios.get(
+      `https://www.themealdb.com/api/json/v1/1/filter.php?i=${isSearched}`
+    );
+    console.log(res.data.meals[0]);
   };
 
   return (
-    <form onSubmit={submitHandler}>
-      <input
-        type="text"
-        style={{ padding: "18px", borderRadius: "25px" }}
-        onInput={(e) => isSetSearched(e.target.value)}
-      ></input>
-      <button type="submit">
-        <SearchIcon />
-      </button>
-    </form>
+    <PageContainer>
+      <ForkSpoonImg src={forkspoon} />
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          style={{ padding: "18px", borderRadius: "25px" }}
+          onInput={(e) => isSetSearched(e.target.value)}
+        ></input>
+        <button type="submit">
+          <SearchIcon />
+        </button>
+      </form>
+    </PageContainer>
   );
 }
