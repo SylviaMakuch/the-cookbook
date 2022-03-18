e5b5af2f06b9bae6b64cb050aaa097d4d4bf3669
Sylvia Makuch <sylvia.makuch@protonmail.com>
2022-03-17 20:43:24 -0700

    ts added and inital commit for Catergories

---
+++ src/components/Categories.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const PageContainer = styled.div`
+  width: 100%;
+`;
+
+function Categories() {
+  return <PageContainer></PageContainer>;
+}
+
+export default Categories;
