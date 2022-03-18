a29a3b78deb693501875cf4bf5050bc6d98bfa48
Sylvia Makuch <sylvia.makuch@protonmail.com>
2022-03-18 12:21:22 -0700

    api connected to search bar and working

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,56 +1,28 @@
 import React from "react";
-// import TextField from "@mui/material/TextField";
+import { useState } from "react";
 import SearchIcon from "@mui/icons-material/Search";
-// import IconButton from "@mui/icons-material/IconButton";
-// import InputAdornment from "@mui/material/InputAdornment";
-import {
-  IconButton,
-  InputAdornment,
-  TextField,
-} from "@mui/material";
 import axios from "axios";
 
 export default function Search() {
-//   const axios = require("axios").default;
+  const [isSearched, isSetSearched] = useState("");
+  
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log(isSearched);
+    const res =  await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${isSearched}`);
+    console.log(res.data);
+  };
 
-//   const options = {
-//     method: "GET",
-//     url: "https://themealdb.p.rapidapi.com/filter.php",
-//     params: { i: "chicken_breast" },
-//     headers: {
-//       "x-rapidapi-host": "themealdb.p.rapidapi.com",
-//       "x-rapidapi-key": "e120e44aebmshc56159d5e1ff737p16b7dfjsn3d53a95184bd",
-//     },
-//   };
-
-//   axios
-//     .request(options)
-//     .then(function (response) {
-//       console.log(response.data);
-//     })
-//     .catch(function (error) {
-//       console.error(error);
-//     });
   return (
-    <TextField
-      id="outlined-search"
-      color="warning"
-      label="Seatch"
-      InputProps={{
-        startAdornment: (
-          <InputAdornment position="end">
-            <IconButton
-              color="primary"
-              aria-label="upload picture"
-              component="span"
-            >
-              <SearchIcon />
-            </IconButton>
-          </InputAdornment>
-        ),
-      }}
-      type="search"
-    ></TextField>
+    <form onSubmit={submitHandler}>
+      <input
+        type="text"
+        style={{ padding: "18px", borderRadius: "25px" }}
+        onInput={(e) => isSetSearched(e.target.value)}
+      ></input>
+      <button type="submit">
+        <SearchIcon />
+      </button>
+    </form>
   );
-  console.dir = TextField.element.query.value;
 }
