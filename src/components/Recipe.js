bc356e7a7533b163bda3935f834d11bc96ff9a33
Sylvia Makuch <sylvia.makuch@protonmail.com>
2022-03-31 15:11:33 -0700

    Update Recipe.js

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -5,18 +5,31 @@ import axios from "axios";
 
 const PageContainer = styled.div`
   width: 100%;
+  background-color: #d0f0ee;
   display: flex;
-  justify-content: center;
   align-items: center;
-  align-content: center;
+  justify-content: space-evenly;
+  flex-direction: row;
+  flex-wrap: wrap;
+  flex-direction: row;
+  margin: 0;
+`;
+
+const InnerContainer = styled.div`
+  display: flex;
   flex-wrap: wrap;
   flex-direction: column;
-  margin: 0;
+  margin: 50px;
+  padding: 50px;
+  border-radius: 50px;
+  background: linear-gradient(225deg, #bbd8d6, #dfffff);
+  box-shadow: -20px 20px 60px #b1ccca, 20px -20px 60px #efffff;
 `;
 
 const Title = styled.h1`
   font-family: "Roboto Slab", serif;
-  font-weight: 300;
+  font-weight: 800;
+  -webkit-font-smoothing: antialiased;
   color: red;
   font-size: 40px;
   letter-spacing: 0.8px;
@@ -33,21 +46,19 @@ const SubTitle = styled.h2`
 
 const Picture = styled.img`
   width: 500px;
+  border-radius: 0px 0px 50px 50px;
+  background-image: linear-gradient(to top, #ff000000, #ff000000, #f9e8e894);
 `;
 
-const IngredientBox = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  flex-direction: column;
-`;
+const IngredientBox = styled.div``;
 
 const QuanityBox = styled.div`
-  display: flex;
+  /* display: flex;
   flex-wrap: wrap;
-  flex-direction: column;
+  flex-direction: column; */
 `;
 
-const Instructions = styled.ul`
+const Instructions = styled.div`
   font-family: "Abril Fatface";
   font-size: 20px;
   width: 700px;
@@ -69,7 +80,6 @@ function createIngredientList(recipe) {
       });
     }
   }
-
   return items;
 }
 
@@ -77,7 +87,7 @@ export default function Recipe() {
   const [recipe, setRecipe] = useState([]);
 
   const fetchRecipe = () => {
-    const url = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52874';
+    const url = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=52874";
     axios
       .get(url)
       .then((res) => {
@@ -96,22 +106,29 @@ export default function Recipe() {
   const items = createIngredientList(recipe);
 
   return (
-    <PageContainer>
-      <Title>{recipe.strMeal}</Title>
-      <Picture src={recipe.strMealThumb} />
-      <SubTitle>
-        Catergories: {recipe.strCategory}, {recipe.strArea}
-      </SubTitle>
-      <IngredientBox>
-        <ul>
-          {items.map((item) => (
-            <li key={item.ingredient}>
-              {item.measure}: {item.ingredient}
-            </li>
-          ))}
-        </ul>
-      </IngredientBox>
-      <Instructions>{recipe.strInstructions}</Instructions>
-    </PageContainer>
+    <>
+      <PageContainer>
+        <InnerContainer>
+          <Title>{recipe.strMeal}</Title>
+          <h3>
+            Catergories: {recipe.strCategory}, {recipe.strArea}
+          </h3>
+          <Picture src={recipe.strMealThumb} />
+        </InnerContainer>
+        <InnerContainer>
+          <IngredientBox>
+            <SubTitle> Ingredients </SubTitle>{" "}
+            <ul>
+              {items.map((item) => (
+                <QuanityBox key={item.ingredient}>
+                  {item.measure} of {item.ingredient}
+                </QuanityBox>
+              ))}
+            </ul>
+          </IngredientBox>
+        </InnerContainer>
+        <Instructions>{recipe.strInstructions}</Instructions>
+      </PageContainer>
+    </>
   );
 }
