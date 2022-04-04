809001044552fbb50b8db699ce8ccdb976a968bf
Sylvia Makuch <sylvia.makuch@protonmail.com>
2022-04-04 00:19:50 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { store } from "./redux/store.js";
-import { Provider } from "react-redux";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById("root")
 );
