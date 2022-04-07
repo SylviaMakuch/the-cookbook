d8d0d58fbb52b2d1272bda4eab8a134102c15844
Sylvia Makuch <sylvia.makuch@protonmail.com>
2022-04-07 15:42:11 -0700

    added scroll to top function for react router dom

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Search from "./components/Search.js";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Meal from "./components/Meal.js";
+import ScrollToTop from "./components/ScrollToTop.js";
 
 
 function App() {
   return (
     <>
       <BrowserRouter>
+      <ScrollToTop />
         <Routes>
           <Route path="/" element={<Search />} />
           {/* <Route path="catergories" element={<Categories />} /> */}
